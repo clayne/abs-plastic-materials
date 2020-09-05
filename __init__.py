48024cdca0ad9abd4c7de120844b7aaaef9187be
Christopher Gearhart <cgear13@gmail.com>
2020-09-05 11:09:58 -0400

    added addon zipperg

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ bl_info = {
     "blender"     : (2, 83, 0),
     "description" : "Append ABS Plastic Materials to current blender file with a simple click",
     "location"    : "PROPERTIES > Materials > ABS Plastic Materials",
-    "warning"     : "Untested Beta release – update to official release when available",  # used for warning icon and text in addons panel
+    "warning"     : "Untested Beta release - update to official release when available",  # used for warning icon and text in addons panel
     "wiki_url"    : "https://www.blendermarket.com/products/abs-plastic-materials",
     "doc_url"     : "https://www.blendermarket.com/products/abs-plastic-materials",  # 2.83+
     "tracker_url" : "https://github.com/bblanimation/abs-plastic-materials/issues",
